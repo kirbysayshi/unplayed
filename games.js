ee03363bfd093f95d9e32e6a90d59475f8f3a675
Drew Petersen <senofpeter@gmail.com>
2020-01-16 23:00:02 -0500

    Update games.js

--- games.js
+++ games.js
@@ -66,6 +66,7 @@ Unplayed(
   "",
   "https://medium.com/@banovg/gregs-top-10-games-of-2019-c0032182a038"
 );
+Unplayed("For the Frog the Bell Tolls", "GB", "Translation patch! http://www.romhacking.net/translations/1623/ Uses the same engine as Link’s Awakening. Richard!", "2019-12-27", "", "https://mobile.twitter.com/waltzforluma/status/1210721896494325768");
 Unplayed("");
 
 UnplayedUnreleased("Nth Dimension[al] Hiking", "PC", "so esoteric so mysterious", "", "", "");
