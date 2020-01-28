db8dbcaba0cfd5e0ec0f993ee3edb2af965ae544
Drew Petersen <senofpeter@gmail.com>
2020-01-28 00:09:17 -0500

    Update games.js

--- games.js
+++ games.js
@@ -70,6 +70,7 @@ Unplayed(
 Unplayed("For the Frog the Bell Tolls", "GB", "Translation patch! http://www.romhacking.net/translations/1623/ Uses the same engine as Link’s Awakening. Richard!", "2019-12-27", "", "https://mobile.twitter.com/waltzforluma/status/1210721896494325768");
 Unplayed("Republic Commando", "PC", "There’s a patch to make it look as good as it did at launch.", "", "", "DGC+Brett Douville");
 Unplayed("Life is Strange Ch 2-5", "iOS", "", "2020-01-20", "", "");
+Unplayed("Kentucky Route Zero", "Swtich/PS4", "Waited for the final version instead of early access!", "2020-01-28", "", "");
 Unplayed("");
 
 UnplayedUnreleased("Nth Dimension[al] Hiking", "PC", "so esoteric so mysterious", "", "", "");
