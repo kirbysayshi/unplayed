3b47ec4a446b61f3dd219fb385b407822f56b6b6
Andrew Petersen <kirbysayshi@gmail.com>
2020-04-06 23:14:08 -0400

    rebuild

--- out/site-data.js
+++ out/site-data.js
@@ -3,7 +3,7 @@
 const owner = "kirbysayshi";
 const repo = "unplayed";
 const metadata = {
-    about: `<a href="https://kirbysayshi.com">Drew Petersen</a> tried this around 2010 via Trello (blech!), fell off, and is trying again. <a href="https://github.com/kirbysayshi/unplayed">Make your own</a> if you'd like!`,
+    about: `<a href="https://kirbysayshi.com">Drew Petersen</a> tried this around 2012 via Trello (blech!), fell off, and is trying again. <a href="https://github.com/kirbysayshi/unplayed">Make your own</a> if you'd like!`,
     editLink: `https://github.com/${owner}/${repo}/edit/gh-pages/games.js`,
 };
 export function useMetadata() {
