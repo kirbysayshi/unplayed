3c7a9c78a1cc5a971d6668f4005e8ba60dd4dc16
Drew Petersen <senofpeter@gmail.com>
2025-01-27 02:15:54 -0500

    Update games.js

--- games.js
+++ games.js
@@ -1,4 +1,7 @@
 
+prepend("NnzaPSvz5yereju4cPlUq", "log", "[{\"text\":\"Aiming to just play ME1 but we’ll see what happens.\",\"date\":\"2024-12-28\"},{\"text\":\"Finished ME1 in 29h36m! Paragon run, saved the council.\",\"date\":\"2025-01-02\"},{\"text\":\"Oops I’m a ME2 space miner again! I chose Sentinel… is that the same as my first playthrough on PS3? I can’t remember!\",\"date\":\"2025-01-03\"},{\"text\":\"Credits on ME2, skipped some of the N7 DLC. Everybody lived!\",\"date\":\"2025-01-26\"}]");
+
+
 prepend("7MFV7Slo36wZN01M2gOsb", "status", "Beaten");
 prepend("7MFV7Slo36wZN01M2gOsb", "name", "Universal Paperclips");
 prepend("7MFV7Slo36wZN01M2gOsb", "platform", "PC");
